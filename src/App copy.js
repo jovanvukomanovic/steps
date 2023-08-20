03239a2c4b91cb8f1ccffd5b973a7fff28211e0f
jovanVukomanovic <105781991+jovanvukomanovic@users.noreply.github.com>
2023-08-20 07:34:31 +0200

    simple-step-app

---
+++ src/App copy.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePreviousClick() {
+    if (step > 1) setStep((prev) => prev - 1);
+  }
+  function handleNextClick() {
+    if (step < 3) setStep((prev) => prev + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">Step: {messages[step - 1]}</p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePreviousClick}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNextClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Steps />
     </div>
   );
 }
 
-export default App;
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePreviousClick() {
+    if (step > 1) setStep((prev) => prev - 1);
+  }
+  function handleNextClick() {
+    if (step < 3) setStep((prev) => prev + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          {/* <p className="message">Step: {messages[step - 1]}</p> */}
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          {/* <StepMessage step={"magnificent"}>
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onHandleClick={() => {
+                  alert(`your alerted message is here ${step + 1}`);
+                }}
+              >
+                push the button
+              </Button>
+            </div>
+          </StepMessage> */}
+
+          <div className="buttons">
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onHandleClick={handlePreviousClick}
+            >
+              <span>👈</span>
+              Previus
+            </Button>
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onHandleClick={handleNextClick}
+            >
+              Next<span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step: {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ onHandleClick, bgColor, textColor, children }) {
+  return (
+    <button
+      onClick={() => onHandleClick()}
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      {children}
+    </button>
+  );
+}
